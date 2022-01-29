c240278cc3887847a6fc47705cfa546151ac6f1a
t_kodama <>
2022-01-29 17:25:06 +0900

    axiosでのデータ取得

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
+/* import Vue from 'vue' */
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+/* import axios from 'axios'
+import VueAxios from 'vue-axios' */
 
-createApp(App).use(router).mount('#app')
+/* Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios) */
+
+/* new Vue({
+  router,
+  render: h=>h(App)
+}).mount('#app')
+*/
+
+createApp(App).use(router, axios).mount('#app')
--- src/views/Pages/_id.vue
+++ src/views/Pages/_id.vue
@@ -2,13 +2,39 @@
   <div>
     {{ $route.params.id }}
     aaaz
+    <div v-for="item in info" v-bind:key="item.key">
+      {{ item }}
+    </div>
+    {{ info }}
     <router-link :to="{ name: 'PagesProfile', params: { id: $route.params.id } }">プロフィールページ</router-link>
     <router-link :to="{ name: 'PagesAbout', params: { id: $route.params.id } }">詳細ページ</router-link>
     <router-view></router-view>
   </div>
 </template>
 <script>
+  import axios from 'axios'
+
   export default {
-    name: 'Pages'
+    name: 'Pages',
+    /* async asyncData(){
+      const {data} = await axios.get('https://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060')
+      return data
+    }*/
+    data(){
+      return {
+        info: null
+      }
+    },
+    mounted() {
+      this.setting()
+    },
+    methods: {
+      setting: async function() {
+        const res = await axios.get('https://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060')
+        /* const item = JSON.parse("'"&res.data.results[0]&"'")*/
+        /* this.info = items.address1 */
+        this.info=res.data.results[0]
+      }
+    }
   }
 </script>
