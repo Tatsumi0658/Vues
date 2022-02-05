69ca1e5b39dc7b91a207c2a4557ef9ae26a15139
t_kodama <>
2022-02-06 00:03:48 +0900

    scss導入とBEM記法housei

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    {{ count }}
+    <ul class="card-list">
+      <li class="card-list__card">
+        <p class="card__card-title">
+          タイトル
+        </p>
+        <div class="card__card-body">
+          <p class="card-body__text">
+            テキストテキストテキスト
+          </p>
+          <button class="card-body__link">
+            詳しくはこちら
+          </button>
+        </div>
+      </li>
+      <li class="card-list__card--bggray">
+        <p class="card__card-title--white">
+          タイトル
+        </p>
+        <div class="card__card-body">
+          <p class="card-body__text--red">
+            テキストテキストテキスト
+          </p>
+          <button class="card-body__link">
+            詳しくはこちら
+          </button>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
   /* computed: {
@@ -17,8 +40,43 @@ export default {
       return this.$store.state.count
     }
   },*/
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style lang="scss">
+ul {
+  list-style: none;
+}
+
+%card {
+  border: 1px solid #ccc;
+  padding: 16px;
+}
+
+.card-list__card {
+  @extend %card;
+  &--bggray {
+    @extend %card;
+    background-color: #ccc;
+  }
+}
+
+.card__card-body {
+  padding: 8px;
+  & .card-body__text {
+    font-size: 18px;
+  }
+  & .card-body__link {
+    border: 1px solid blue;
+    border-radius: 12px;
+    padding: 8px;
+    background-color: blue;
+    color: #fff;
+  }
+}
+.card-body__text {
+  &--red {
+    color: red;
+  }
+}
+</style>
